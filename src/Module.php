038d78f0b803d5e323e1988a56a018b888f3cfb8
Arturo <ircsasw@gmail.com>
2023-08-21 20:05:39 -0500

    build bootstrapping

---
+++ src/Module.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the ircsasw/yii2-cms project.
+ *
+ * (c) IRCSA Software! <http://ircsasoftware.com.mx/>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Ircsasw\Cms;
+
+use yii\base\Module as BaseModule;
+
+/**
+ * This is the main module class of the yii2-usuario extension.
+ */
+class Module extends BaseModule
+{
+
+}
---
+++ src/controllers/CmsController.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the ircsasw/yii2-cms project.
+ *
+ * (c) IRCSA Software! <http://ircsasoftware.com.mx/>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Ircsasw\Cms\controllers;
+
+use yii\web\Controller;
+
+class CmsController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Algo
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index', [
+        ]);
+    }
+}
